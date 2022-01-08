6f79d318b71161313b423145e4f5fd1b214756b4
Sebastian Coroian <sebicoroian@outlook.com>
2022-01-08 02:56:21 +0200

    finalfinalfinalfinalultimatefinal

---
+++ Real_Estate_Agency/Models/Contacte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Npgsql;
+
+namespace Real_Estate_Agency.Models
+{
+    public class Contacte
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string nume { get; set; }
+        public string nr_tel { get; set; }
+        public string mail { get; set; }
+
+
+        public DateTime date { get; set; }
+    }
+}
